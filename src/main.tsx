877ec738f851ffaf4d370f56061d39cd8db004e5
retr0lbb <retr0lbb@gmail.com>
2025-11-08 18:53:35 -0300

    feat: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router"
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {App} from './App.tsx'
+import { MyRouter } from './router/router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <MyRouter/>
+    </BrowserRouter>
+  </StrictMode>
 )
